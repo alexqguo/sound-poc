f51aaaf944944c1249b0cffad0901066bf150e00
Alex Guo <alexguo@amazon.com>
2020-07-18 17:09:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import WaveformPlaylist from 'waveform-playlist';
 
 function App() {
+  useEffect(() => {
+    const playlist = WaveformPlaylist({
+      samplesPerPixel: 3000,
+      mono: true,
+      waveHeight: 70,
+      container: document.getElementById('playlist'),
+      state: 'cursor',
+      colors: {
+        waveOutlineColor: '#E0EFF1',
+        timeColor: 'grey',
+        fadeColor: 'black'
+      },
+      controls: {
+        show: true,
+        width: 200
+      },
+      zoomLevels: [500, 1000, 3000, 5000]
+    });
+
+    playlist
+      .load([
+        {
+          src: 'tracks/alto.wav',
+          name: 'alto',
+          gain: 0.5
+        },
+        {
+          src: 'tracks/bari.wav',
+          name: 'bari'
+        },
+      ])
+      .then(() => {
+        console.log('Loaded')
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="playlist"></div>
   );
 }
 
