aeb0430b4188bca564d97593324855d3a9c6d938
Alex Guo <alexguo@amazon.com>
2020-08-01 14:58:39 -0400

    It can kinda record a bit

--- src/App.js
+++ src/App.js
@@ -11,59 +11,81 @@ const loadingStates = Object.freeze({
 function App() {
   const [loadingState, setLoadingState] = useState(loadingStates.loading);
   const [playlist, setPlaylist] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [stream, setStream] = useState(null);
 
   useEffect(() => {
-    const playlist = WaveformPlaylist({
-      samplesPerPixel: 3000,
-      mono: true,
-      waveHeight: 70,
-      container: document.getElementById('playlist'),
-      state: 'cursor',
-      colors: {
-        waveOutlineColor: '#E0EFF1',
-        timeColor: 'grey',
-        fadeColor: 'black'
-      },
-      controls: {
-        show: true,
-        width: 200
-      },
-      zoomLevels: [500, 1000, 3000, 5000]
-    });
-
-    playlist
-      .load([
-        {
-          src: 'tracks/alto.wav',
-          name: 'alto',
-          gain: 0.5
+    (async function() {
+      const playlist = WaveformPlaylist({
+        samplesPerPixel: 3000,
+        waveHeight: 120,
+        container: document.getElementById('playlist'),
+        state: 'cursor',
+        colors: {
+          waveOutlineColor: '#E0EFF1',
+          timeColor: 'grey',
+          fadeColor: 'black'
         },
-        {
-          src: 'tracks/bari.wav',
-          name: 'bari'
+        controls: {
+          show: true,
+          width: 200
         },
-        {
-          src: 'tracks/tenor.wav',
-          name: 'tenor'
-        }
-      ])
-      .then(() => {
-        const ee = playlist.getEventEmitter();
-        setLoadingState(loadingStates.loaded);
-        setPlaylist(playlist);
-        ee.on('timeupdate', (time) => console.log(time));
+        zoomLevels: [500, 1000, 3000, 5000]
       });
+      window.p = playlist;
+  
+      await playlist.load([
+        { src: 'tracks/alto.wav', name: 'alto' },
+        { src: 'tracks/bari.wav', name: 'bari' },
+        { src: 'tracks/tenor.wav', name: 'tenor' },
+      ]);
+        
+      setPlaylist(playlist);
+      setLoadingState(loadingStates.loaded);
+      // const ee = playlist.getEventEmitter();
+      // ee.on('timeupdate', (time) => console.log(time));
+
+      // window.addEventListener('keydown', (e) => {
+      //   if (e.keyCode === 32) togglePlaying();
+      // });
+
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        playlist.initRecorder(stream);
+        setStream(stream);
+      } catch (e) {
+        console.error('Access to microphone not granted by user', e);
+      }
+    })();
   }, []);
 
-  // record example: https://github.com/naomiaro/waveform-playlist/blob/master/ghpages/js/record.js
+  const togglePlaying = () => {
+    if (playlist.isPlaying() && isPlaying) {
+      setIsPlaying(false);
+      playlist.pause();
+    } else {
+      setIsPlaying(true);
+      playlist.play();
+    }
+  };
 
+  // record example: https://github.com/naomiaro/waveform-playlist/blob/master/ghpages/js/record.js
   return (
     <main>
       {
         loadingState === loadingStates.loaded ? <section>
           <ButtonGroup size="sm">
-            <Button onClick={() => playlist.play()}>Play</Button>
-            <Button onClick={() => playlist.pause()}>Pause</Button>
+            <Button onClick={togglePlaying}>
+              {isPlaying ? 'pause' : 'play'}
+            </Button>
+
+            <Button variant="success" disabled={!stream} onClick={() => playlist.getEventEmitter().emit('record')}>
+              Record
+            </Button>
+
+            <Button variant="danger" onClick={() => playlist.stop()}>
+              Stop
+            </Button>
           </ButtonGroup>
         </section> : null
       }
